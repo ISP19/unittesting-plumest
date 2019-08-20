23e2a33e5158fad972e6e19163f8c54e36d072d5
plumest <chayathon.k@ku.th>
2019-08-20 17:55:33 +0700

    Add __str__ && __eq__

--- fraction.py
+++ fraction.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Fraction:
     """A fraction with a numerator and denominator and arithmetic operations.
 
@@ -7,23 +10,34 @@ class Fraction:
     unique representation, e.g. 4/5, 24/30, and -20/-25 have the same
     internal representation.
     """
-    
+
     def __init__(self, numerator, denominator=1):
         """Initialize a new fraction with the given numerator
            and denominator (default 1).
         """
-        #TODO write this (and remove this TODO comment)
-        pass
+        self.numerator = numerator
+        self.denominator = denominator
+        self.gcd = math.gcd(abs(self.numerator), abs(self.denominator))
 
-    #TODO Write the __add__ method, and remove this TODO comment.
+    def __str__(self):
+        if self.numerator == 0:
+            return '0'
+        elif self.numerator % self.denominator == 0:
+            return f'{int(self.numerator/self.denominator)}'
+        elif self.numerator / self.denominator < 0:
+            return f'-{int(self.numerator / self.gcd)}/{int(abs(self.denominator / self.gcd))}'
+        else:
+            return f'{int(abs(self.numerator / self.gcd))}/{int(abs(self.denominator / self.gcd))}'
+
+    # TODO Write the __add__ method, and remove this TODO comment.
     def __add__(self, frac):
         """Return the sum of two fractions as a new fraction.
            Use the standard formula  a/b + c/d = (ad+bc)/(b*d)
         """
         pass
 
-    #TODO write __mul__ and __str__.  Verify __eq__ works with your code.
-    #Optional have fun and overload other operators such as 
+    # TODO write __mul__ and __str__.  Verify __eq__ works with your code.
+    # Optional have fun and overload other operators such as
     # __sub__ for f-g
     # __gt__  for f > g
     # __neg__ for -f (negation)
@@ -33,4 +47,5 @@ class Fraction:
            Fractions are stored in proper form so the internal representation
            is unique (3/6 is same as 1/2).
         """
-        return self.numerator == frac.numerator and self.denominator == frac.denominator
+        if self.__str__() == frac.__str__():
+            return True
